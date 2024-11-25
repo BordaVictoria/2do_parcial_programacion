2adf8342de28a97324274fdbb5be7db55d629f32
bordavictoria <bordavictoria@hotmail.com>
2024-11-25 08:13:04 -0300

    ni ideaa

---
+++ Package/funciones_pygame.py
@@ -0,0 +1,36 @@
+import pygame as pg
+
+def crear_boton(ventana: pg.Surface, posicion, dimension, texto, fuente, color, color_fondo, bandera=True) -> dict:
+    boton = {}
+    boton["pantalla"] = ventana
+    boton["posicion"] = posicion
+    boton["dimension"] = dimension
+    boton["Presionado"] = False
+
+    fuente_texto = pg.font.SysFont(fuente[0], fuente[1])
+    boton["superficie"] = pg.Surface(dimension)
+    boton["superficie"].fill(color_fondo)
+    boton["texto"] = texto
+    texto_renderizado = fuente_texto.render(texto, True, color)
+    texto_rect = texto_renderizado.get_rect(center=(dimension[0] // 2, dimension[1] // 2))
+    boton["superficie"].blit(texto_renderizado, texto_rect)
+
+    boton["rectangulo"] = pg.Rect(posicion, dimension)
+    if bandera:
+        boton["rectangulo"].topleft = boton["posicion"]
+
+    return boton
+
+def dibujar_boton(boton: dict):
+    boton["pantalla"].blit(boton["superficie"], boton["rectangulo"])
+
+def mostrar_matriz_p(ventana: pg.Surface, matriz, fuente, color, color_fondo):
+    boton_ancho, boton_alto = 50, 50
+    x, y = 200, 100
+    for fila in matriz:
+        for elemento in fila:
+            boton = crear_boton(ventana, (x, y), (boton_ancho, boton_alto), str(elemento), fuente, color, color_fondo, False)
+            dibujar_boton(boton)
+            x += 50  # Increment x by the button width plus margin to avoid overlap
+        x = 200  # Reset x to initial position after each row
+        y += 50  # Increment y by the button height plus margin to avoid overlap
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 from Package.funciones import *
 from Package.juego import *
 import pygame as pg
+from Package.funciones_pygame import *
 
 pg.init()
 
@@ -16,43 +17,55 @@ colores = {
     "gris": (189, 192, 201)
 }
 
-
-def main():
-    tupla = ("descubrir continente","descubrir letra", "descubrir comida")
-    usos_comodines = [0] *len(tupla)
+def main_juego():
+    pg.init()
+    reloj = pg.time.Clock()
+    tamaño_ventana = (700, 400)
+    ventana = pg.display.set_mode(tamaño_ventana)
+    pg.display.set_caption("Palabrini")
+    fuente = ("Arial", 20)
+    fuente_matriz = ("Arial", 40)
+    dificultad = [5,6,7]
+    intentos = 6
     palabras = obtener_lista_palabras("words.csv")
     palabras_normalizadas = normalizar_en_diccionario(palabras)
-    lista_puntuacion = []
-    palabras_adivinadas = 0
-    tiempo_rondas = []
-    intentos_partidas_perdidas = 0
-    intentos = 6
-    
-    #region Bucle principal del juego
-    
-    while palabras_adivinadas < 5 and intentos_partidas_perdidas < 3:
-        dificultad = pedir_dificultad("Que dificultad quiere jugar ? 5 / 6 / 7 : ","Porfavor ingrese 5, 6 o 7")
-        palabra_generada = obtener_palabra(palabras_normalizadas,dificultad)
-        matriz = generar_matriz(palabra_generada,intentos)
-        print(palabra_generada)
-        #region Bucle de rondas
-        tiempo_inicio = time.time()
+    palabra_generada = obtener_palabra(palabras_normalizadas,dificultad[0])
+    matriz = generar_matriz(palabra_generada,intentos)
+    bandera_juego = True
+    boton_modos = []
+    posicion_botones_x = 100
+    posicion_botones_y = 0
+    palabra_obtenida = ""
+    for i in range(3):
+        boton = crear_boton(ventana, (posicion_botones_x, posicion_botones_y), (200, 100), f"{5+i} caracteres", fuente, colores["negro"], colores["blanco"])
+        boton_modos.append(boton)
+        posicion_botones_x += 150
+    while bandera_juego:
+        for evento in pg.event.get():
+            if evento.type == pg.QUIT:
+                bandera_juego = False
+            if evento.type == pg.MOUSEBUTTONDOWN:
+                if boton_modos[i]["rectangulo"].collidepoint(evento.pos):
+                    matriz_pantalla = boton_modos[i]["texto"]
+                    palabra_generada = obtener_palabra(palabras_normalizadas,matriz_pantalla)
+                    matriz = generar_matriz(palabra_generada,intentos)
+                    mostrar_matriz_p(ventana,matriz,fuente,"blue","grey")
         
-        puntuacion_modificada = jugar(palabra_generada, tupla, usos_comodines, matriz, intentos)
+        for i in range(3):
+            dibujar_boton(boton_modos[i])
         
-        tiempo_final = time.time()
-        tiempo_rondas.append(tiempo_final - tiempo_inicio)
-        lista_puntuacion.append(puntuacion_modificada)
-        print(puntuacion_modificada)
-        if validar_estado(puntuacion_modificada) :
-            palabras_adivinadas += 1
-        else:
-            intentos_partidas_perdidas += 1 
-        os.system("pause")
-        os.system("cls")
-    #endregion
-    guardar_puntuacion(tiempo_rondas,palabras_adivinadas,lista_puntuacion, usos_comodines)
-    os.system("pause")
-
-
-main()
+        ventana.fill(colores["blanco"])
+        
+            
+        
+        
+                
+        reloj.tick(20)
+        pg.display.update()
+    
+    
+    
+    pg.quit()
+    
+    
+main_juego()
